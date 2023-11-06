eb39ab893b3130df960f302d3d03e1ad1dcff013
ppincon <pierre.pincon@irit.fr>
2023-11-06 17:11:32 +0100

    add Task.py

---
+++ Task.py
@@ -0,0 +1,21 @@
+import time
+
+import numpy as np
+
+
+class Task:
+    def __init__(self, identifier, size=None):
+        self.identifier = identifier
+        # choose the size of the problem
+        self.size = size or np.random.randint(300, 3_000)
+        # Generate the input of the problem
+        self.a = np.random.rand(self.size, self.size)
+        self.b = np.random.rand(self.size)
+        # prepare room for the results
+        self.x = np.zeros((self.size))
+        self.time = 0
+
+    def work(self):
+        start = time.perf_counter()
+        self.x = np.linalg.solve(self.a, self.b)
+        self.time = time.perf_counter() - start
