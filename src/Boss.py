764701df77dbd734c17ee00eb6fcb9e2ce538da7
Pierre Pinçon <74105302+PinconP@users.noreply.github.com>
2023-11-24 11:11:31 +0100

    Update Boss.py
    It's now a class which can send and wait for responses.

--- src/Boss.py
+++ src/Boss.py
@@ -1,10 +1,32 @@
+import queue
+import time
+
 from Manager import QueueClient
 from Task import Task
 
+
+class Boss:
+    def submit_task(self, task_id, task_size):
+        task = Task(task_id, task_size)
+        client.tasks.put(task)
+        print(f"Boss added task {task_id} to the queue.")
+
+    def wait_for_results(self, task_id):
+        while True:
+            try:
+                result_id, result_time = client.results.get_nowait()
+                print(f"Boss received result {result_id} in {result_time:.4f} seconds")
+            except queue.Empty:
+                print("Result queue is empty. Boss is sleeping.")
+                time.sleep(5)
+                continue
+            if result_id == task_id:
+                break
+
+
 if __name__ == "__main__":
     client = QueueClient()
-
-    for i in range(5):  # Let's say we want to put 5 tasks
-        client.tasks.put(Task(i, 6000))
-
-    print("Boss has added tasks to the queue.")
+    boss = Boss()
+    boss.submit_task(0, 6000)
+    boss.wait_for_results(0)
+    print("Boss is done.")
