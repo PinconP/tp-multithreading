5b8ac59d9d3b84eb5ed84facd8a0c4e387e018e0
Pierre Pinçon <74105302+PinconP@users.noreply.github.com>
2023-11-24 11:52:20 +0100

    Fin du troisième TP
    Modification Task.py et ajout du slide c-tp-3.pdf

--- src/Task.py
+++ src/Task.py
@@ -1,6 +1,9 @@
+import json
+import unittest
 from time import perf_counter
 
-import numpy as np  # Importation de la bibliothèque NumPy pour les opérations sur les tableaux
+# Importation de la bibliothèque NumPy pour les opérations sur les tableaux
+import numpy as np
 
 
 class Task:
@@ -13,7 +16,8 @@ class Task:
         self.a = np.random.rand(
             self.size, self.size
         )  # Matrice A aléatoire de dimensions size x size
-        self.b = np.random.rand(self.size)  # Vecteur B aléatoire de dimension size
+        # Vecteur B aléatoire de dimension size
+        self.b = np.random.rand(self.size)
 
         # Initialiser un vecteur pour stocker les résultats, initialisé à des zéros
         self.x = np.zeros((self.size))
@@ -27,3 +31,74 @@ class Task:
         self.time = (
             perf_counter() - start
         )  # Calculer le temps écoulé pour la résolution
+
+    def to_json(self) -> str:
+        # Convertir les attributs NumPy en listes pour la sérialisation
+        data = {
+            "identifier": self.identifier,
+            "size": self.size,
+            "a": self.a.tolist(),  # Convertir la matrice NumPy en liste
+            "b": self.b.tolist(),  # Convertir le vecteur NumPy en liste
+            "x": self.x.tolist(),  # Convertir le vecteur de résultat en liste
+            "time": self.time,
+        }
+        return json.dumps(data)  # Sérialiser le dictionnaire en chaîne JSON
+
+    @classmethod
+    def from_json(cls, text: str) -> "Task":
+        # Désérialiser le texte JSON en un dictionnaire
+        data = json.loads(text)
+
+        # Créer une instance de Task en utilisant les données du dictionnaire
+        task = cls(identifier=data["identifier"], size=data["size"])
+
+        # Convertir les listes en matrices/vecteurs NumPy
+        task.a = np.array(data["a"])
+        task.b = np.array(data["b"])
+        task.x = np.array(data["x"])
+        task.time = data["time"]
+
+        return task
+
+    def __eq__(self, other: "Task") -> bool:
+        if not isinstance(other, Task):
+            # Retourner False si 'other' n'est pas une instance de Task
+            return False
+
+        # Comparer l'identifiant, la taille et le temps
+        are_basic_attrs_equal = (
+            self.identifier == other.identifier
+            and self.size == other.size
+            and self.time == other.time
+        )
+
+        if not are_basic_attrs_equal:
+            return False
+
+        # Comparer les matrices et les vecteurs en utilisant np.array_equal
+        are_arrays_equal = (
+            np.array_equal(self.a, other.a)
+            and np.array_equal(self.b, other.b)
+            and np.array_equal(self.x, other.x)
+        )
+
+        return are_arrays_equal
+
+
+class TestTask(unittest.TestCase):
+    def test_serialization(self):
+        # Création d'une instance de Task
+        a = Task("1")
+
+        # Sérialisation de l'instance 'a'
+        a_json = a.to_json()
+
+        # Création d'une nouvelle instance de Task à partir de la sérialisation
+        b = Task.from_json(a_json)
+
+        # Vérification de l'égalité entre 'a' et 'b'
+        self.assertEqual(a, b)
+
+
+if __name__ == "__main__":
+    unittest.main()
