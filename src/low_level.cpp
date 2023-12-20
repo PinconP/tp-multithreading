746e57b929203a7747dbefa1b80e5a81edf72211
Pierre Pinçon <pierre.pincon@univ-tlse3.fr>
2023-12-20 11:55:58 +0100

    Update low_level.cpp
    More or less final. Works pretty much like to Python version but doesn't put the response in a response queue.

--- src/low_level.cpp
+++ src/low_level.cpp
@@ -6,47 +6,57 @@
 #include <nlohmann/json.hpp>
 
 class Minion {
+ private:
+  Eigen::MatrixXd a;
+  Eigen::VectorXd b;
+  Eigen::VectorXd x;
+  int id;
+
  public:
-  void work() {
+  Minion() : id(-1) {}
+
+  bool init() {
     cpr::Response r = cpr::Get(cpr::Url{"http://localhost:8000"});
-    // Vérifier si la requête a réussi
-    if (r.status_code == 200) {
-      // Parser la réponse en un objet JSON
-      nlohmann::json jsonResponse = nlohmann::json::parse(r.text);
+    if (r.status_code != 200) {
+      return false;
+    }
 
-      int size =
-          jsonResponse["size"];  // Utilisez la taille de 'a' dans le JSON
-      Eigen::MatrixXd a(size, size);
-      Eigen::VectorXd b(size);
-      Eigen::VectorXd x(size);
+    nlohmann::json jsonResponse = nlohmann::json::parse(r.text);
+    id = jsonResponse["identifier"];
+    int size = jsonResponse["size"];
+    a.resize(size, size);
+    b.resize(size);
+    x.resize(size);
 
-      // Remplir la matrice 'a'
-      for (int i = 0; i < size; ++i) {
-        for (int j = 0; j < size; ++j) {
-          a(i, j) = jsonResponse["a"][i][j];
-        }
+    for (int i = 0; i < size; ++i) {
+      for (int j = 0; j < size; ++j) {
+        a(i, j) = jsonResponse["a"][i][j];
       }
+      b(i) = jsonResponse["b"][i];
+    }
 
-      // Remplir le vecteur 'b'
-      for (int i = 0; i < size; ++i) {
-        b(i) = jsonResponse["b"][i];
-      }
+    return true;
+  }
 
-      auto start = std::chrono::high_resolution_clock::now();
+  void work() {
+    auto start = std::chrono::high_resolution_clock::now();
+    x = a.colPivHouseholderQr().solve(b);
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+    double time = elapsed.count();
+    std::cout << "Time taken: " << time << " seconds\n";
+  }
 
-      x = a.colPivHouseholderQr().solve(b);
-
-      auto end = std::chrono::high_resolution_clock::now();
-      std::chrono::duration<double> elapsed = end - start;
-      double time = elapsed.count();
-      std::cout << "Time taken: " << time << " seconds\n";
+  void executeTasks() {
+    while (init()) {
+      work();
     }
   }
 };
 
 int main() {
   Minion minion;
-  minion.work();
+  minion.executeTasks();
 
   return 0;
 }
