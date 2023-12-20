8cda4eb35295f8fad58cea96ae4fdc4e0ad3d993
Pierre Pinçon <pierre.pincon@univ-tlse3.fr>
2023-12-20 11:17:19 +0100

    Update low_level.cpp
    Now does a single task

--- src/low_level.cpp
+++ src/low_level.cpp
@@ -1,22 +1,52 @@
 #include <cpr/cpr.h>
 
+#include <Eigen/Dense>
+#include <chrono>
 #include <iostream>
 #include <nlohmann/json.hpp>
 
-int main() {
-  cpr::Response r = cpr::Get(cpr::Url{"http://localhost:8000"});
+class Minion {
+ public:
+  void work() {
+    cpr::Response r = cpr::Get(cpr::Url{"http://localhost:8000"});
+    // Vérifier si la requête a réussi
+    if (r.status_code == 200) {
+      // Parser la réponse en un objet JSON
+      nlohmann::json jsonResponse = nlohmann::json::parse(r.text);
 
-  // Vérifier si la requête a réussi
-  if (r.status_code == 200) {
-    // Parser la réponse en un objet JSON
-    nlohmann::json jsonResponse = nlohmann::json::parse(r.text);
+      int size =
+          jsonResponse["size"];  // Utilisez la taille de 'a' dans le JSON
+      Eigen::MatrixXd a(size, size);
+      Eigen::VectorXd b(size);
+      Eigen::VectorXd x(size);
 
-    // Afficher l'objet JSON
-    std::cout << "Response JSON:\n" << jsonResponse.dump(4) << std::endl;
-  } else {
-    std::cout << "Failed to get response, status code: " << r.status_code
-              << std::endl;
+      // Remplir la matrice 'a'
+      for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+          a(i, j) = jsonResponse["a"][i][j];
+        }
+      }
+
+      // Remplir le vecteur 'b'
+      for (int i = 0; i < size; ++i) {
+        b(i) = jsonResponse["b"][i];
+      }
+
+      auto start = std::chrono::high_resolution_clock::now();
+
+      x = a.colPivHouseholderQr().solve(b);
+
+      auto end = std::chrono::high_resolution_clock::now();
+      std::chrono::duration<double> elapsed = end - start;
+      double time = elapsed.count();
+      std::cout << "Time taken: " << time << " seconds\n";
+    }
   }
+};
+
+int main() {
+  Minion minion;
+  minion.work();
 
   return 0;
 }
