4c6eb562d9fdef98d797f03edd25d900b70e8af0
Pierre Pinçon <pierre.pincon@univ-tlse3.fr>
2023-12-20 09:08:23 +0100

    Update low_level.cpp

--- src/low_level.cpp
+++ src/low_level.cpp
@@ -1,6 +1,22 @@
+#include <cpr/cpr.h>
+
 #include <iostream>
+#include <nlohmann/json.hpp>
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  cpr::Response r = cpr::Get(cpr::Url{"http://localhost:8000"});
+
+  // Vérifier si la requête a réussi
+  if (r.status_code == 200) {
+    // Parser la réponse en un objet JSON
+    nlohmann::json jsonResponse = nlohmann::json::parse(r.text);
+
+    // Afficher l'objet JSON
+    std::cout << "Response JSON:\n" << jsonResponse.dump(4) << std::endl;
+  } else {
+    std::cout << "Failed to get response, status code: " << r.status_code
+              << std::endl;
+  }
+
   return 0;
 }
